1657aa5b21d5de18db808d29fa8519349ffd49f4
김태헌 <kimtaehun@gimtaeheon-ui-MacBookAir.local>
2024-04-08 17:01:03 +0900

    arr

---
+++ BOJ10808.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+char input[101];
+
+void alpha(char arr[]);
+
+void alpha(char arr[]) {
+
+}
+
+int main(void) {
+    ios::sync_with_stdio(0);
+    cin >> input;
+    alpha(input);
+    return 0;
+}
