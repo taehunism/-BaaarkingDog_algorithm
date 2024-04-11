9444330b6052a2cb69738a7f3bdb45167325616c
김태헌 <kimtaehun@gimtaeheon-ui-MacBookAir.local>
2024-04-11 12:56:04 +0900

    .

--- 0x01/BOJ10093.cpp
+++ 0x01/BOJ10093.cpp
@@ -1,4 +1,5 @@
-#include <bits/stdc++.h>
+//#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
 
 int main(void) {
@@ -9,10 +10,14 @@ int main(void) {
     long long b = 0;
     int diff = 0;
     cin >> a >> b;
-    if (a > b) swap(a,b)
+    if (a > b) swap(a,b);
     if (a == b || b - a == 1) cout << 0;
-    
+    else {
     cout << b-a-1 << '\n';
 
+    for (long long i = 0; i<b-a-1;i++) {
+        cout << a+i+1 << " ";
+    }
+    }
     return 0;
 }
--- 0x01/BOJ2309.cpp
+++ 0x01/BOJ2309.cpp
@@ -1,10 +1,30 @@
-#include <bits/stdc++.h>
+//#include <bits/stdc++.h>
+#include <iostream>
+#include <algorithm>
 using namespace std;
+
+int height[9];
 
 int main(void) {
     ios::sync_with_stdio(0);
     cin.tie(0);
-    
-    int 
+
+    int total = 0;
+    int err = 0;
+    for (int i = 0; i<9; i++) {
+        cin >> height[i];
+        total = total + height[i];
+    }
+
+    sort(height, height + 9);
+    err = total - 100;
+
+    for (int i = 0; i<9; i++) {
+        if(err == height[i] + height[9-i-1]) {
+            
+        }
+    }
+
+
     return 0;
 }
